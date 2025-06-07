5208db656d46be0a7529dc60a14bdb8f466b5e3e
Rio Fujita <rio_github@rio.st>
2025-06-08 08:54:14 +0900

    Update videowallpaper.rb

--- Casks/videowallpaper.rb
+++ Casks/videowallpaper.rb
@@ -1,6 +1,6 @@
 cask "videowallpaper" do
-  version "1.0.0"
-  sha256 "dd5ede854d1e64736149b9565fa1fa06d7a0f86df01c5e6b80f4b3dd825a3ef9"
+  version "1.0.2"
+  sha256 "78a146b22b3ca85ab00760e2e0f155da7482d0f29e37038b8c992ea5e554d1c6"
 
   url "https://github.com/rioriost/homebrew-videowallpaper/releases/download/#{version}/VideoWallpaper-#{version}.zip"
   name "VideoWallpaper"
